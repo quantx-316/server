2163204b0c3a54e276f5a0a6cf01433a09faa49c
Joon Young Lee <joonyoung.lee@duke.edu>
2021-10-10 13:40:32 -0400

    router ex added

--- app/main.py
+++ app/main.py
@@ -7,24 +7,9 @@ from app.db import get_db
 
 app = FastAPI()
 
-@app.post("/users/", response_model=schemas.User)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = models.User.get_user_by_email(db, email=user.email)
-    if db_user: 
-        raise HTTPException(status_code=400, detail="Email already registered")
-    return models.User.create_user(db=db, user=user)
+from .routers import users 
 
-@app.get("/users/", response_model=List[schemas.User])
-def get_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    users = models.User.get_users(db, skip=skip, limit=limit)
-    return users 
-
-@app.get("/user/{user_id}", response_model=schemas.User)
-def get_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = models.User.get_user(db, user_id=user_id)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail='User not found')
-    return db_user 
+app.include_router(users.router)
 
 @app.get("/quote/{symbol}/all", response_model=List[schemas.Quote])
 def get_all_quotes_for_symbol(symbol: str, db: Session = Depends(get_db)):
