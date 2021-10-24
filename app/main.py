c488dbe6c6428e7e0740bdba550e8325c31a41e8
Joon Young Lee <joonyoung.lee@duke.edu>
2021-10-23 23:02:56 -0400

    CORS middleware required for all api interaction

--- app/main.py
+++ app/main.py
@@ -1,26 +1,24 @@
-from fastapi import Depends, FastAPI
-from app.db import get_db
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware, 
+    allow_origins=origins, 
+    allow_credentials=True, 
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# import needs to be here or crash 
 from .routers import users, security, algos
 
 app.include_router(users.router)
 app.include_router(security.router)
 app.include_router(algos.router)
-
-# # get symbols endpoint 
-
-# @app.get("/quote/{symbol}/all", response_model=List[schemas.Quote])
-# def get_all_quotes_for_symbol(symbol: str, db: Session = Depends(get_db)):
-#     quotes = models.Quote.get_all_quotes_for_symbol(db, symbol=symbol)
-#     if quotes is None:
-#         raise HTTPException(status_code=404, detail='Symbol not found')
-#     return quotes
-
-# @app.get("/quote/{symbol}", response_model=schemas.Quote)
-# def get_single_quote(symbol: str, time: datetime, db: Session = Depends(get_db)):
-#     quote = models.Quote.get_single_quote(db, symbol=symbol, time=time)
-#     if quote is None:
-#         raise HTTPException(status_code=404, detail='Quote not found')
-#     return quote
